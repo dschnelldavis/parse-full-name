01823ccbb5561d7808407651345b805afff39296
David Schnell-Davis <David@DMBP.local>
2016-06-25 14:47:36 -0700

    Initial commit function parseFullName() version 1.0.0

---
+++ README.md
@@ -0,0 +1,133 @@
+# parse-full-name
+
+## Description
+
+parseFullName() is designed to parse large batches of full names in multiple
+inconsistent formats, as from a database, and continue processing without error,
+even if given some unparsable garbage entries.
+
+parseFullName():
+1. accepts a string containing a person's full name, in any format,
+2. analyzes and attempts to detect the format of that name,
+3. (if possible) parses the name into its component parts, and
+4. (by default) returns an object containing all individual parts of the name:
+      title (string): title (e.g. "Ms." or "Dr.")
+      first (string): first name or initial
+      middle (string): middle name or initial
+      last (string): last name or initial
+      nick (string): nickname
+      suffix (string): suffix (e.g. "Jr.", "II", or "Esq.")
+      error (array of strings): any parsing error messages
+
+Optionally, parseFullName() can also
+* return only the specified part of a name as a string
+* always fix or ignore the letter case of the returned parts (the default is
+    to fix the case only when the original input is all upper or all lowercase)
+* stop on errors (the default is to return warning messages in the output,
+    but never throw a JavaScript error, no matter how mangled the input)
+* detect more variations of name prefixes, suffixes, and titles (the default
+    detects 29 prefixes, 19 suffixes, 16 titles, and 8 conjunctions, but it
+    can be set to detect 97 prefixes, 23 suffixes, and 204 titles instead)
+
+If this is not what you're looking for, is overkill for your application, or
+is in the wrong language, check the "Credits" section at the end of this file
+for links to several other excellent parsers which may suit your needs better.
+
+## Use
+
+### Basic Use
+
+```javascript
+var parseFullName = require('parse-full-name').parseFullName;
+
+name = parseFullName('Mr. David Davis');
+assert.equal(name.title, 'Mr.');
+assert.equal(name.first, 'David');
+assert.equal(name.last, 'Davis');
+```
+
+### Options
+
+parseFullName(nameToParse, partToReturn, fixCase, stopOnError, useLongLists)
+
+nameToParse (string, required): the name to be parsed
+
+partToReturn (string, optional): the name of a single part to return
+  'all' (default) = return an object containing all name parts
+  'title' = return only the title as a string (or an empty string)
+  'first' = return only the first name as a string (or an empty string)
+  'middle' = return only the middle name as a string (or an empty string)
+  'last' = return only the last name as a string (or an empty string)
+  'nick' = return only the nickname as a string (or an empty string)
+  'suffix' = return only the suffix as a string (or an empty string)
+  'error' = return only the array of parsing error messages (or an empty array)
+
+fixCase (integer, optional): fix case of output name
+  0 = never fix the case, retain and output same case as input name
+  -1 (default) = fix case only if input name is all upper or lowercase
+  1 = always fix case of output, even if input is mixed case
+
+stopOnError (integer, optional): makes parsing errors throw JavaScript errors
+  0 (default) = return warnings about potential parsing errors, but continue
+  1 = if a parsing error is found, throw a JavaScript error
+
+useLongLists (integer, optional): use long prefix, suffix, and title lists
+  0 (default) = use default lists (29 prefixes, 19 suffixes, and 16 titles)
+  1 = use experimental long lists (97 prefixes, 23 suffixes, and 204 titles)
+  Note: The alternate long lists are experimental and have not been tested.
+  Be especially careful using the long prefix list, which may incorrectly
+  detect "Ben" as a prefix, which is common in middle-eastern names,
+  rather than as a first name, which is common in English names
+
+## Credits and precursors
+
+Before creating this function I studied many other name-parsing functions.
+None quite suited my needs, but many are excellent at what they do, and
+this function uses ideas from several of them.
+
+My thanks to all the following developers for sharing their work.
+
+"If I have seen further, it is by standing on the shoulders of giants."
+— Isaac Newton
+
+Josh Fraser's PHP-Name-Parser:
+https://github.com/joshfraser/PHP-Name-Parser
+Josh Fraser's JavaScript-Name-Parser:
+https://github.com/joshfraser/JavaScript-Name-Parser
+
+Garve Hays' Java NameParser:
+https://github.com/gkhays/NameParser
+
+Jason Priem's PHP HumanNameParser:
+https://web.archive.org/web/20150408022642/http://jasonpriem.org/human-name-parse/
+https://github.com/jasonpriem/HumanNameParser.php
+
+Keith Beckman's PHP nameparse:
+http://alphahelical.com/code/misc/nameparse/
+
+Jed Hartman's PHP normalize_name:
+http://www.kith.org/journals/jed/2007/02/11/3813.html
+http://www.kith.org/logos/things/code/name-parser-php.html
+
+ashaffer88's JavaScript parse-name:
+https://github.com/weo-edu/parse-name
+https://www.npmjs.com/package/parse-name
+
+Derek Gulbranson's Python nameparser:
+https://github.com/derek73/python-nameparser/
+
+Discussion about how to change all upper or lowercase names to correct case:
+http://stackoverflow.com/questions/11529213/given-upper-case-names-transform-to-proper-case-handling-ohara-mcdonald
+
+Title lists modified from:
+http://www.codeproject.com/Questions/262876/Titles-or-Salutation-list
+
+Suffix lists modified from:
+http://en.wikipedia.org/wiki/Suffix_(name) and
+https://github.com/derek73/python-nameparser/blob/master/nameparser/config/suffixes.py
+
+Prefix lists modified from:
+http://en.wikipedia.org/wiki/List_of_family_name_affixes
+
+Conjunction list copied entirely from:
+https://github.com/derek73/python-nameparser/blob/master/nameparser/config/conjunctions.py
---
+++ test/index.js
@@ -0,0 +1,105 @@
+var assert = require('assert');
+var parseFullName = require('../').parseFullName;
+var error;
+
+var verifyName = function(nameToCheck, partsToCheck) {
+  assert.equal(nameToCheck.title, partsToCheck[0]);
+  assert.equal(nameToCheck.first, partsToCheck[1]);
+  assert.equal(nameToCheck.middle, partsToCheck[2]);
+  assert.equal(nameToCheck.last, partsToCheck[3]);
+  assert.equal(nameToCheck.nick, partsToCheck[4]);
+  assert.equal(nameToCheck.suffix, partsToCheck[5]);
+  assert.equal(nameToCheck.error.length, partsToCheck[6].length);
+  for (var i=1;i<partsToCheck[6].length;i++) {
+    assert.equal(nameToCheck.error[i], partsToCheck[6][i]);
+  }
+};
+
+describe('parse-full-name', function() {
+  describe('parseFullName', function() {
+    it('parses first names', function() {
+      verifyName(parseFullName('David Davis'),['','David','','Davis','','',[]]);
+      verifyName(parseFullName('Davis, David'),['','David','','Davis','','',[]]);
+    });
+    it('parses last names', function() {
+      verifyName(parseFullName('Gerald Böck'),['','Gerald','','Böck','','',[]]);
+      verifyName(parseFullName('Böck, Gerald'),['','Gerald','','Böck','','',[]]);
+    });
+    it('parses middle names', function() {
+      verifyName(parseFullName('David William Davis'),['','David','William','Davis','','',[]]);
+      verifyName(parseFullName('Davis, David William'),['','David','William','Davis','','',[]]);
+    });
+    it('parses last names including known prefixes', function() {
+      verifyName(parseFullName('Vincent Van Gogh'),['','Vincent','','Van Gogh','','',[]]);
+      verifyName(parseFullName('Van Gogh, Vincent'),['','Vincent','','Van Gogh','','',[]]);
+      verifyName(parseFullName('Lorenzo de Médici'),['','Lorenzo','','de Médici','','',[]]);
+      verifyName(parseFullName('de Médici, Lorenzo'),['','Lorenzo','','de Médici','','',[]]);
+      verifyName(parseFullName('Jüan de la Véña'),['','Jüan','','de la Véña','','',[]]);
+      verifyName(parseFullName('de la Véña, Jüan'),['','Jüan','','de la Véña','','',[]]);
+    });
+    it('parses compound last names', function() {
+      verifyName(parseFullName('Jüan Martinez de Lorenzo y Gutierez'),['','Jüan','Martinez','de Lorenzo y Gutierez','','',[]]);
+      verifyName(parseFullName('de Lorenzo y Gutierez, Jüan Martinez'),['','Jüan','Martinez','de Lorenzo y Gutierez','','',[]]);
+    });
+    it('parses nicknames', function() {
+      verifyName(parseFullName('Orenthal James "O. J." Simpson'),['','Orenthal','James','Simpson','O. J.','',[]]);
+      verifyName(parseFullName("Orenthal 'O. J.' James Simpson"),['','Orenthal','James','Simpson','O. J.','',[]]);
+      verifyName(parseFullName('(O. J.) Orenthal James Simpson'),['','Orenthal','James','Simpson','O. J.','',[]]);
+      verifyName(parseFullName('Simpson, Orenthal James “O. J.”'),['','Orenthal','James','Simpson','O. J.','',[]]);
+      verifyName(parseFullName("Simpson, Orenthal ‘O. J.’ James"),['','Orenthal','James','Simpson','O. J.','',[]]);
+      verifyName(parseFullName('Simpson, [O. J.] Orenthal James'),['','Orenthal','James','Simpson','O. J.','',[]]);
+    });
+    it('parses known suffixes', function() {
+      verifyName(parseFullName('Sammy Davis, Jr.'),['','Sammy','','Davis','','Jr.',[]]);
+      verifyName(parseFullName('Davis, Sammy, Jr.'),['','Sammy','','Davis','','Jr.',[]]);
+    });
+    it('parses unknown suffixes', function() {
+      verifyName(parseFullName('John P. Doe-Ray, Jr., CLU, CFP, LUTC'),['','John','P.','Doe-Ray','','Jr., CLU, CFP, LUTC',[]]);
+      verifyName(parseFullName('Doe-Ray, John P., Jr., CLU, CFP, LUTC'),['','John','P.','Doe-Ray','','Jr., CLU, CFP, LUTC',[]]);
+    });
+    it('parses titles', function() {
+      verifyName(parseFullName('Dr. John P. Doe-Ray, Jr.'),['Dr.','John','P.','Doe-Ray','','Jr.',[]]);
+      verifyName(parseFullName('Dr. Doe-Ray, John P., Jr.'),['Dr.','John','P.','Doe-Ray','','Jr.',[]]);
+      verifyName(parseFullName('Doe-Ray, Dr. John P., Jr.'),['Dr.','John','P.','Doe-Ray','','Jr.',[]]);
+    });
+    it('parses name parts in many different orders', function() {
+      verifyName(parseFullName('Mr. Jüan Martinez (Martin) de Lorenzo y Gutierez Jr.'),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+      verifyName(parseFullName('de Lorenzo y Gutierez, Mr. Jüan Martinez (Martin) Jr.'),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+      verifyName(parseFullName('de Lorenzo y Gutierez, Mr. Jüan (Martin) Martinez Jr.'),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+      verifyName(parseFullName('Mr. de Lorenzo y Gutierez, Jüan Martinez (Martin) Jr.'),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+      verifyName(parseFullName('Mr. de Lorenzo y Gutierez, Jüan (Martin) Martinez Jr.'),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+      verifyName(parseFullName('Mr. de Lorenzo y Gutierez Jr., Jüan Martinez (Martin)'),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+      verifyName(parseFullName('Mr. de Lorenzo y Gutierez Jr., Jüan (Martin) Martinez'),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+      verifyName(parseFullName('Mr. de Lorenzo y Gutierez, Jr. Jüan Martinez (Martin)'),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+      verifyName(parseFullName('Mr. de Lorenzo y Gutierez, Jr. Jüan (Martin) Martinez'),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+    });
+    it('automatically fixes all upper and all lowercase names', function() {
+      verifyName(parseFullName('MR. JÜAN MARTINEZ (MARTIN) DE LORENZO Y GUTIEREZ JR.'),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+      verifyName(parseFullName('mr. jüan martinez (martin) de lorenzo y gutierez jr.'),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+    });
+    it('manually fixes case, or not, when specified', function() {
+      verifyName(parseFullName('Mr. JÜAN MARTINEZ (MARTIN) DE LORENZO Y GUTIEREZ Jr.'),['Mr.','JÜAN','MARTINEZ','DE LORENZO Y GUTIEREZ','MARTIN','Jr.',[]]);
+      verifyName(parseFullName('Mr. JÜAN MARTINEZ (MARTIN) DE LORENZO Y GUTIEREZ JR.','all',1),['Mr.','Jüan','Martinez','de Lorenzo y Gutierez','Martin','Jr.',[]]);
+      verifyName(parseFullName('mr. jüan martinez (martin) de lorenzo y gutierez jr.','all',0),['mr.','jüan','martinez','de lorenzo y gutierez','martin','jr.',[]]);
+    });
+    it('returns a single part, when specified', function() {
+      assert.equal(parseFullName('Mr. Jüan Martinez (Martin) de Lorenzo y Gutierez Jr.','title'),'Mr.');
+      assert.equal(parseFullName('Mr. Jüan Martinez (Martin) de Lorenzo y Gutierez Jr.','first'),'Jüan');
+      assert.equal(parseFullName('Mr. Jüan Martinez (Martin) de Lorenzo y Gutierez Jr.','middle'),'Martinez');
+      assert.equal(parseFullName('Mr. Jüan Martinez (Martin) de Lorenzo y Gutierez Jr.','last'),'de Lorenzo y Gutierez');
+      assert.equal(parseFullName('Mr. Jüan Martinez (Martin) de Lorenzo y Gutierez Jr.','nick'),'Martin');
+      assert.equal(parseFullName('Mr. Jüan Martinez (Martin) de Lorenzo y Gutierez Jr.','suffix'),'Jr.');
+    });
+    it('continues processing, even when fed garbage input', function() {
+      verifyName(parseFullName('as;dfkj ;aerha;sfa ef;oia;woeig hz;sofi hz;oif j;zoseifj zs;eofij z;soeif jzs;oefi jz;osif z;osefij zs;oif jz;soefi hz;sodifh z;sofu hzsieufh zlsiudfh zksefiulzseofih ;zosufh ;oseihgf z;osef h:OSfih lziusefhaowieufyg oaweifugy'),['','as;dfkj',';aerha;sfa ef;oia;woeig hz;sofi hz;oif j;zoseifj zs;eofij z;soeif jzs;oefi jz;osif z;osefij zs;oif jz;soefi hz;sodifh z;sofu hzsieufh zlsiudfh zksefiulzseofih ;zosufh ;oseihgf z;osef h:OSfih lziusefhaowieufyg','oaweifugy','','',['Error: 22 middle names']]);
+    });
+    it('returns warnings for null/undefined names', function() {
+      verifyName(parseFullName(null),['','','','','','',['Error: No input']]);
+      verifyName(parseFullName(),['','','','','','',['Error: No input']]);
+    });
+    it('will throw errors, when specified', function() {
+      assert.doesNotThrow(function(){return parseFullName('');});
+      assert.throws(function(){return parseFullName('','all',-1,1);});
+    });
+  });
+});
